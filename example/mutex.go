d1cc0ac019b5f65c66ae1363fcf809a904396783
Chayangkoon <chayangkoondokhom@gmail.com>
2022-02-13 11:13:33 +0700

    change mutex

--- example/mutex.go
+++ example/mutex.go
@@ -6,34 +6,33 @@ import (
 	"time"
 )
 
-// SafeCounter is safe to use concurrently.
-type SafeCounter struct {
-	mu sync.Mutex
-	v  map[string]int
-}
-
-// Inc increments the counter for the given key.
-func (c *SafeCounter) Inc(key string) {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	c.v[key]++
-	c.mu.Unlock()
-}
-
-// Value returns the current value of the counter for the given key.
-func (c *SafeCounter) Value(key string) int {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	defer c.mu.Unlock()
-	return c.v[key]
-}
-
 func main() {
-	c := SafeCounter{v: make(map[string]int)}
-	for i := 0; i < 1000; i++ {
-		go c.Inc("somekey")
-	}
+	i := &SafeNumber{}
+	go func() {
+		i.Set(5)
+	}()
 
-	time.Sleep(time.Second)
-	fmt.Println(c.Value("somekey"))
+	fmt.Println(i.Get())
+
+	time.Sleep(1 * time.Second)
+	fmt.Println(i.Get())
+}
+
+type SafeNumber struct {
+	val int
+	m   sync.Mutex
+}
+
+func (i *SafeNumber) Get() int {
+	i.m.Lock()
+	defer i.m.Unlock()
+
+	return i.val
+}
+
+func (i *SafeNumber) Set(val int) {
+	i.m.Lock()
+	defer i.m.Unlock()
+	
+	i.val = val
 }
